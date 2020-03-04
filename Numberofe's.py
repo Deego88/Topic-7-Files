837febb8fa011218113937131afc28227495771b
Richard Deegan <G00387896@gmit.ie>
2020-03-04 11:44:38 +0000

    topic 7 with info

--- Numberofe's.py
+++ Numberofe's.py
@@ -1,13 +1,13 @@
 #Richard Deegan
 #This program reads in a text file and outputs the number of e's it contains.
-x = str("e)")
+x = str("e")
 
 with open ("es.py moby-dick.txt", "r") as myfile:
-    data = myfile.read().replace('\n', '')
+    data = myfile.read().replace('\n', '') #create data variable to be searched, each line
     print(data)
     freq = 0
-    for char in data:
-        if char == x:
-            freq = freq + 1
+    for char in data: #creates a for loop, 
+        if char == x: #conditional statement if character is equal to x variable (e)
+            freq = freq + 1 #add one ot the freq of occurence 
     print(freq)
 
